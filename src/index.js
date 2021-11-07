ee71d0c4e119cd21661a6b143c5facf8cf6b6fbe
Geoffroy Couprie <contact@geoffroycouprie.com>
2021-11-07 15:01:16 +0100

    proper imports

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import './bc-token-editor.js';
+import './bc-token-content.js';
+import './bc-authorizer-editor.js';
+import './bc-authorizer-result.js';
+import './bc-authorizer-content.js';
+export {default, execute} from "@geal/biscuit-component-wasm";
