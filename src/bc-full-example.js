c6147a1fc69c6db7aa45c23ae483758d9c592b13
Geoffroy Couprie <contact@geoffroycouprie.com>
2021-12-01 22:48:30 +0100

    remove dead code

--- src/bc-full-example.js
+++ src/bc-full-example.js
@@ -177,60 +177,6 @@ export class BcFullExample extends LitElement {
         <bc-authorizer-content content='${JSON.stringify(result.authorizer_world)}'></bc-authorizer-content>
       </details>
     `;
-
-    /*
-
-    if(this.started) {
-
-      authorizer_result = result.authorizer_result;
-      authorizer_world = result.authorizer_world;
-
-      if(result.authorizer_editor != null) {
-
-        for(let error of result.authorizer_editor.errors) {
-          parseErrors.push({
-            message: error.message,
-            severity: "error",
-            line_start: error.position.line_start,
-            from: error.position.start,//CodeMirror.Pos(error.position.line_start, error.position.column_start),
-            to: error.position.end,//CodeMirror.Pos(error.position.line_end, error.position.column_end),
-          });
-        }
-
-        for(let marker of result.authorizer_editor.markers) {
-          console.log(marker);
-          markers.push({
-            from: {
-              line: marker.position.line_start,
-              ch: marker.position.column_start,
-            },
-            to: {
-              line: marker.position.line_end,
-              ch: marker.position.column_end,
-            },
-            start: marker.position.start,
-            end: marker.position.end,
-            ok: marker.ok,
-          });
-        }
-      }
-    }
-
-    return html`
-      <bc-authorizer-editor
-        code='${this.code}'
-        parseErrors='${JSON.stringify(parseErrors)}'
-        markers='${JSON.stringify(markers)}'
-        @bc-authorizer-editor:update="${(e) => { this._onUpdatedCode(e.detail.code) }}"}>
-      </bc-authorizer-editor>
-      <em>Execution result</em>
-      <bc-authorizer-result content='${JSON.stringify(authorizer_result)}'></bc-authorizer-result>
-      <details>
-        <summary>Facts</summary>
-        <bc-authorizer-content content='${JSON.stringify(authorizer_world)}'></bc-authorizer-content>
-      </details>
-    `;
-      */
   }
 
   static get styles () {
